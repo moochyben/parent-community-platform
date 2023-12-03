760e78b8e0cc21c1dbf587c47b9701e707f5cb0a
moochyben <145360214+moochyben@users.noreply.github.com>
2023-12-03 13:03:01 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// JavaScript for handling login and signup
+
+document.addEventListener('DOMContentLoaded', function() {
+    var loginBtn = document.getElementById('loginBtn');
+    var signupBtn = document.getElementById('signupBtn');
+
+    if (loginBtn) {
+        loginBtn.addEventListener('click', function() {
+            alert('Login functionality to be implemented.');
+        });
+    }
+
+    if (signupBtn) {
+        signupBtn.addEventListener('click', function() {
+            alert('Signup functionality to be implemented.');
+        });
+    }
+});
